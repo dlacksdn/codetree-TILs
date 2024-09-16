054de41d692b53db9548829eacd04fc9edc65966
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2024-09-16 21:06:52 +0900

    Title: 개인정보, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240916/개인정보/personal-info.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+class Process {
+public:
+	string name;
+	int height;
+	double weight;
+	
+
+	Process(string nm, int h, double w) {
+		name = nm;
+		height = h;
+		weight = w;
+	}
+	Process() {}
+};
+
+bool isValueName(const Process& a, const Process& b) {
+	return a.name < b.name;
+}
+
+bool isValueHeight(const Process& a, const Process& b) {
+	return a.height > b.height;
+}
+
+int main() {
+	int n = 5;
+
+	string name;
+	int height;
+	double weight;
+	vector<Process> v(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> name >> height >> weight;
+		v[i] = Process(name, height, weight);
+	}
+	
+	sort(v.begin(), v.end(), isValueName);
+
+	for (int i = 0; i < n; i++) {
+		cout << fixed << setprecision(1);
+		cout << v[i].name << " ";
+		cout << v[i].height << " ";
+		cout << v[i].weight << "\n";
+	}
+
+	sort(v.begin(), v.end(), isValueHeight);
+
+	for (int i = 0; i < n; i++) {
+		cout << fixed << setprecision(1);
+		cout << v[i].name << " ";
+		cout << v[i].height << " ";
+		cout << v[i].weight << "\n";
+	}
+}
