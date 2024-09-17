6fbd99eb74dcda7915add4e53677751f130dbda7
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2024-09-17 18:07:52 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240917/그 요일은/the-day-of-the-day.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int m1, d1, m2, d2;
+	cin >> m1 >> d1 >> m2 >> d2;
+
+	string target;
+	cin >> target;
+
+	int arr[13] = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	string day[7] = { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+
+	int start = 0;
+	for (int i = 1; i < m1; i++) {
+		start += arr[i];
+	}
+	start += d1;
+
+	int end = 0;
+	for (int i = 1; i < m2; i++) {
+		end += arr[i];
+	}
+	end += d2;
+
+	int num;
+	for (int i = 0; i < 7; i++) {
+		if (day[i] == target) {
+			num = i;
+			break;
+		}
+	}
+
+	int result = end - start - num;
+	cout << result / 7 + 1;
+
+	
+}
