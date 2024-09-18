f11c73eaa47c23e8f7a3674a3fef921de9c82a6c
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2024-09-18 18:24:51 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240918/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ 240918/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -1,42 +1,49 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <algorithm>
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
+	int n;
+	cin >> n;
 
-    map<int, int> m;  // 위치별 방문 횟수 기록
-    vector<pair<int, char>> v(n);  // 명령 저장
+	map<pair<int, int>, int> m; // 그라운드
+	vector<pair<int, char>> v(n); // 거리 방향 저장
 
-    // 명령 입력 받기
-    for (int i = 0; i < n; i++) {
-        cin >> v[i].first >> v[i].second;
-    }
+	
+	for (int i = 0; i < n; i++) {
+		cin >> v[i].first >> v[i].second;
+	}
 
-    int index = 0;  // 현재 위치
-    for (int i = 0; i < n; i++) {
-        if (v[i].second == 'R') {
-            // 오른쪽으로 v[i].first 만큼 이동
-            for (int j = 0; j < v[i].first; j++) {
-                m[index++]++;
-            }
-        } else if (v[i].second == 'L') {
-            // 왼쪽으로 v[i].first 만큼 이동
-            for (int j = 0; j < v[i].first; j++) {
-                m[index--]++;
-            }
-        }
-    }
+	int index = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < v[i].first; j++) {
+			int start = index;
 
-    // 2번 이상 지나간 구역의 크기 계산
-    int count = 0;
-    for (const auto& pair : m) {
-        if (pair.second >= 2) {
-            count++;
-        }
-    }
+			if (v[i].second == 'R') {
+				index++;
+			}
+			else if (v[i].second == 'L') {
+				index--;
+			}
 
-    cout << count << endl;
+			int end = index;
+
+			int startNum = min(start, end);
+			int endNum = max(start, end);
+
+			m[{startNum, endNum}]++;
+		}
+	}
+
+	int count = 0;
+	for (auto pair : m) {
+		if (pair.second >= 2) {
+			count++;
+		}
+	}
+
+	cout << count;
+
 }
