4659643bacb600b1acdc39d3707830dbf4425b03
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2024-09-18 16:26:11 +0900

    Title: 최대로 겹치는 지점, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240918/최대로 겹치는 지점/maximum-overlapped-points.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define plus 100
+
+int main() {
+	int n;
+	cin >> n;
+
+	vector<pair<int, int>> v(n);
+	int arr[201] = { 0, };
+
+	int left, right;
+	for (int i = 0; i < n; i++) {
+		cin >> left >> right;
+		v[i].first = left + plus;
+		v[i].second = right + plus;
+	}
+
+	for (int i = 0; i < v.size(); i++) {
+		for (int j = v[i].first; j <= v[i].second; j++) {
+			arr[j]++;
+		}
+	}
+
+	auto result = max_element(arr, arr + 201);
+	cout << *result;
+}
