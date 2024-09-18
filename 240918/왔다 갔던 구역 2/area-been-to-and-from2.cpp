954cbf304dc7f1f630011e54e2cab3bc42d583c8
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2024-09-18 18:00:38 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240918/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ 240918/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -1,78 +1,42 @@
 #include <iostream>
 #include <vector>
 #include <map>
-#include <algorithm>
-#include <iterator>
 using namespace std;
 
 int main() {
-	int n;
-	cin >> n;
+    int n;
+    cin >> n;
 
-	map<int, int> m; // 그라운드
-	vector<pair<int, char>> v(n); // 거리 방향 저장
+    map<int, int> m;  // 위치별 방문 횟수 기록
+    vector<pair<int, char>> v(n);  // 명령 저장
 
-	
-	for (int i = 0; i < n; i++) {
-		cin >> v[i].first >> v[i].second;
-	}
+    // 명령 입력 받기
+    for (int i = 0; i < n; i++) {
+        cin >> v[i].first >> v[i].second;
+    }
 
-	int index = 0;
-	for (int i = 0; i < n; i++) {
+    int index = 0;  // 현재 위치
+    for (int i = 0; i < n; i++) {
+        if (v[i].second == 'R') {
+            // 오른쪽으로 v[i].first 만큼 이동
+            for (int j = 0; j < v[i].first; j++) {
+                m[index++]++;
+            }
+        } else if (v[i].second == 'L') {
+            // 왼쪽으로 v[i].first 만큼 이동
+            for (int j = 0; j < v[i].first; j++) {
+                m[index--]++;
+            }
+        }
+    }
 
-		if (v[i].second == 'R') {
-			for (int j = 0; j < v[i].first; j++) {
-				m[index++]++;
-			}
-			m[index]++;
-		}
+    // 2번 이상 지나간 구역의 크기 계산
+    int count = 0;
+    for (const auto& pair : m) {
+        if (pair.second >= 2) {
+            count++;
+        }
+    }
 
-		
-		else if (v[i].second == 'L') {
-			for (int j = 0; j < v[i].first; j++) {
-				m[index--]++;
-			}
-			m[index]++;
-		}
-	}
-
-	int result = 0;
-	//for (auto it = m.begin(); it != m.end(); ++it) {
-	//	const auto& pair = *it; // 현재 요소
-
-	//	// 현재 요소의 다음 요소에 접근
-	//	auto next_it = next(it);
-
-	//	// 다음 요소가 존재하는지 확인하고 조건 체크
-	//	if (next_it != m.end()) {
-	//		if (pair.second >= 2 && (*next_it).second >= 2) {
-	//			count++;
-	//		}
-	//	}
-	//	else {
-	//		if (pair.second >= 2) {
-	//			count++;
-	//		}
-	//	}
-	//}
-
-	int count = 0;
-	bool flag = false;
-	for (auto pair : m) {
-		if (pair.second >= 2) {
-			flag = true;
-			count++;
-		}
-		else if (flag) {
-			count--;
-			flag = false;
-		}
-	}
-
-	if (flag) {
-		count--;
-	}
-
-	cout << count;
-
+    cout << count << endl;
 }
