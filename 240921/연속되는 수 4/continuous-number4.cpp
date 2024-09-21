07f3846530ec3ef2fb8f7bd7e8dcdfa0ee8206dd
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2024-09-21 22:36:55 +0900

    Title: 연속되는 수 4, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240921/연속되는 수 4/continuous-number4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	vector<int> v(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+
+	int count = 1;
+	int maxNum = 0;
+	for (int i = 0; i < n; i++) {
+		if (i == 0 || v[i - 1] >= v[i]) {
+			maxNum = max(count, maxNum);
+			count = 1;
+		}
+		else {
+			count++;
+		}
+		maxNum = max(count, maxNum);
+	}
+
+	cout << maxNum;
+}
