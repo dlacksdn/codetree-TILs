dd38312d4295575cd2bd979c451b975719d730ec
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2024-09-21 22:29:30 +0900

    Title: 연속되는 수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240921/연속되는 수 2/continuous-number2.cpp
+++ 240921/연속되는 수 2/continuous-number2.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+
+#define MAX_N 1000
+
 using namespace std;
 
+int n;
+int arr[MAX_N];
+
 int main() {
-	int n;
-	cin >> n;
-
-	vector<int> v(n);
-	for (int i = 0; i < n; i++) {
-		cin >> v[i];
-	}
-
-	int count = 1;
-	int maxNum = 0;
-	for (int i = 0; i < n; i++) {
-		if (i == 0 || v[i - 1] != v[i]) {
-			maxNum = max(count, maxNum);
-			count = 1;
-		}
-		else {
-			count++;
-		}
-		maxNum = max(count, maxNum);
-	}
-
-	cout << maxNum;
+    // 입력
+    cin >> n;
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    // 연속하여 동일한 숫자가 나오는 횟수를 구해보며,
+    // 그 중 최댓값을 갱신합니다.
+    int ans = 0, cnt = 0;
+    for(int i = 0; i < n; i++) {
+		// Case 1
+        if(i >= 1 && arr[i] == arr[i - 1])
+            cnt++;
+		// Case 2
+        else
+            cnt = 1;
+        
+        ans = max(ans, cnt);
+    }
+    
+    cout << ans;
+    return 0;
 }
