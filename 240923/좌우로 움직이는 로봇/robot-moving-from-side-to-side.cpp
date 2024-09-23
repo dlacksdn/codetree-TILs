cae3ac0ea6456ddb483c3312195d9a82733ee55b
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2024-09-23 21:29:01 +0900

    Title: 좌우로 움직이는 로봇, Time: 50ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240923/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+
+	vector<int> A_Location;
+	vector<int> B_Location;
+	A_Location.push_back(0); B_Location.push_back(0);
+
+	int A_index = 0;
+	for (int i = 0; i < n; i++) {
+		int t; char d;
+		cin >> t >> d;
+
+		if (d == 'L') {
+			for (int j = 0; j < t; j++) {
+				A_Location.push_back(--A_index);
+			}
+		}
+		else {
+			for (int j = 0; j < t; j++) {
+				A_Location.push_back(++A_index);
+			}
+		}
+	}
+
+	int B_index = 0;
+	for (int i = 0; i < m; i++) {
+		int t; char d;
+		cin >> t >> d;
+
+		if (d == 'L') {
+			for (int j = 0; j < t; j++) {
+				B_Location.push_back(--B_index);
+			}
+		}
+		else {
+			for (int j = 0; j < t; j++) {
+				B_Location.push_back(++B_index);
+			}
+		}
+	}
+	int rangeMin = 0;
+	int rangeMax = 0;
+	char flag = ' ';
+	if (A_Location.size() > B_Location.size()) {
+		rangeMin = B_Location.size();
+		rangeMax = A_Location.size();
+		flag = 'A';
+	}
+	else {
+		rangeMin = A_Location.size();
+		rangeMax = B_Location.size();
+		flag = 'B';
+	}
+
+	int count = 0;
+	for (int i = 1; i < rangeMin; i++) {
+		if (A_Location[i - 1] != B_Location[i - 1] && A_Location[i] == B_Location[i]) {
+			count++;
+		}
+	}
+
+	if (flag == 'A') {
+		int num = B_Location[rangeMin - 1];
+		for (int i = rangeMin; i < rangeMax; i++) {
+			if (A_Location[i - 1] != num && A_Location[i] == num) {
+				count++;
+			}
+		}
+	}
+	else if(flag == 'B') {
+		int num = A_Location[rangeMin - 1];
+		for (int i = rangeMin; i < rangeMax; i++) {
+			if (B_Location[i - 1] != num && B_Location[i] == num) {
+				count++;
+			}
+		}
+	}
+	cout << count;
+
+}
