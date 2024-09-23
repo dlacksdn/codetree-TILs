68a9c576fb1be31d9a4624bb823487d034548abb
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2024-09-23 21:48:41 +0900

    Title: 좌우로 움직이는 로봇, Time: 51ms, Memory: 10MB, Status: Passed - CodeTree

--- 240923/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
+++ 240923/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -55,43 +55,11 @@ int main() {
 		}
 	}
 
-	/*int rangeMin = 0;
-	int rangeMax = 0;
-	char flag = ' ';
-	if (A_Location.size() > B_Location.size()) {
-		rangeMin = B_Location.size();
-		rangeMax = A_Location.size();
-		flag = 'A';
-	}
-	else {
-		rangeMin = A_Location.size();
-		rangeMax = B_Location.size();
-		flag = 'B';
-	}*/
-
 	int count = 0;
 	for (int i = 1; i < A_Location.size(); i++) {
 		if (A_Location[i - 1] != B_Location[i - 1] && A_Location[i] == B_Location[i]) {
 			count++;
 		}
 	}
-
-	/*if (flag == 'A') {
-		int num = B_Location[rangeMin - 1];
-		for (int i = rangeMin; i < rangeMax; i++) {
-			if (A_Location[i - 1] != num && A_Location[i] == num) {
-				count++;
-			}
-		}
-	}
-	else if(flag == 'B') {
-		int num = A_Location[rangeMin - 1];
-		for (int i = rangeMin; i < rangeMax; i++) {
-			if (B_Location[i - 1] != num && B_Location[i] == num) {
-				count++;
-			}
-		}
-	}*/
 	cout << count;
-
 }
