9ab137b50907edf738a37814e250f0d68e50b7cd
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2024-09-28 22:35:08 +0900

    Title: 되돌아오기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240928/되돌아오기/come-back.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	int x_dir[4] = { 1, -1, 0, 0 };
+	int y_dir[4] = { 0, 0, -1, 1 };
+
+	char direction; int distance;
+	int x = 0, y = 0;
+	int count = 0;
+	bool flag = false;
+	while (N--) {
+		cin >> direction >> distance;
+
+		if (direction == 'E') {
+			for (int i = 0; i < distance; i++) {
+				x += x_dir[0];
+				y += y_dir[0];
+				count++;
+
+				if (x == 0 && y == 0) {
+					flag = true;
+					break;
+				}
+			}
+		}
+		else if (direction == 'W') {
+			for (int i = 0; i < distance; i++) {
+				x += x_dir[1];
+				y += y_dir[1];
+				count++;
+
+				if (x == 0 && y == 0) {
+					flag = true;
+					break;
+				}
+			}
+		}
+		else if (direction == 'S') {
+			for (int i = 0; i < distance; i++) {
+				x += x_dir[2];
+				y += y_dir[2];
+				count++;
+
+				if (x == 0 && y == 0) {
+					flag = true;
+					break;
+				}
+			}
+		}
+		else if (direction == 'N') {
+			for (int i = 0; i < distance; i++) {
+				x += x_dir[3];
+				y += y_dir[3];
+				count++;
+
+				if (x == 0 && y == 0) {
+					flag = true;
+					break;
+				}
+			}
+		}
+
+		if (flag) {
+			break;
+		}
+		
+	}
+
+	if (count != 0) {
+		cout << count;
+	}
+	else {
+		cout << -1;
+	}
+}
