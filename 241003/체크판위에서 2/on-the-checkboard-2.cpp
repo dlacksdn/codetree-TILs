e1518bbdd436951e123183bf4b782b48d40196bc
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2024-10-03 00:16:35 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241003/체크판위에서 2/on-the-checkboard-2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int R, C;
+	cin >> R >> C;
+
+	vector<vector<char>> v(R + 1, vector<char>(C + 1, ' '));
+
+	for (int i = 1; i <= R; i++) {
+		for (int j = 1; j <= C; j++) {
+			cin >> v[i][j];
+		}
+	}
+
+	int state = (v[1][1] == 'W' ? 0 : 1); // 처음이 W면 0 / B면 1
+
+	int count = 0;
+	for (int i = 2; i < R - 1; i++) { // 1행 1열은 어차피 못쓰니 2부터 시작
+		for (int j = 2; j < C - 1; j++) { 
+			for (int k = i + 1; k < R; k++) {
+				for (int l = j + 1; l < C; l++) {
+					if (state == 0) {
+						if (v[i][j] == 'B' && v[k][l] == 'W')
+							count++;
+					}
+					else if (state == 1) {
+						if (v[i][j] == 'W' && v[k][l] == 'B')
+							count++;
+					}
+				}
+			}
+		}
+	}
+
+	cout << count;
+}
