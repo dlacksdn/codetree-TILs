a22a486323c03161edc7b29cf3d5190d235008ba
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2024-10-03 15:12:47 +0900

    Title: 원 모양으로 되어있는 방, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

--- 241003/원 모양으로 되어있는 방/a-room-in-a-circle.cpp
+++ 241003/원 모양으로 되어있는 방/a-room-in-a-circle.cpp
@@ -14,12 +14,10 @@ int main() {
 
 	int result = INT_MAX;
 	for (int i = 0; i < n; i++) { // 어느 방에서 시작하는지
-		int distance = 1;
 		int sum = 0;
-		for (int j = i + 1; j < i + n; j++) { // 목적지
-			int index = j % n;
-			int plus = v[index] * distance++;
-			sum += plus;
+		for (int j = 0; j < n; j++) { // 목적지
+			int distance = (j - i + n) % n;
+			sum += v[j] * distance;
 		}
 
 		result = min(result, sum);
