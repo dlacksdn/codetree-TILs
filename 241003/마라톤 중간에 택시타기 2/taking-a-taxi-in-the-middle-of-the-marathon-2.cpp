5dc86b405d98e3161cc1c8359cd0703407167b82
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2024-10-03 14:24:34 +0900

    Title: 마라톤 중간에 택시타기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241003/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	vector<pair<int, int>> v(N);
+
+	for (int i = 0; i < N; i++) {
+		cin >> v[i].first >> v[i].second;
+	}
+
+	int result = INT_MAX;
+	for (int i = 1; i < N - 1; i++) { // 어느 체크포인트를 건너뛸 것인가
+		int sum = 0;
+		for (int j = 1; j < N; j++) {
+			if (j == i) {
+				j++;
+				sum += abs(v[j].first - v[j - 2].first) + abs(v[j].second - v[j - 2].second);
+			}
+			else
+				sum += abs(v[j].first - v[j - 1].first) + abs(v[j].second - v[j - 1].second);
+		}
+		result = min(result, sum);
+	}
+
+	cout << result;
+}
