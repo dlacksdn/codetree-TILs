3520115da067e0f82372f163324cda9c6840aa2c
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2024-10-03 14:36:51 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241003/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.cpp
+++ 241003/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.cpp
@@ -16,13 +16,15 @@ int main() {
 	int result = INT_MAX;
 	for (int i = 1; i < N - 1; i++) { // 어느 체크포인트를 건너뛸 것인가
 		int sum = 0;
+		int prev_index = 0;
 		for (int j = 1; j < N; j++) {
 			if (j == i) {
-				j++;
-				sum += abs(v[j].first - v[j - 2].first) + abs(v[j].second - v[j - 2].second);
+				continue;
 			}
-			else
-				sum += abs(v[j].first - v[j - 1].first) + abs(v[j].second - v[j - 1].second);
+			else {
+				sum += abs(v[j].first - v[prev_index].first) + abs(v[j].second - v[prev_index].second);
+				prev_index = j;
+			}
 		}
 		result = min(result, sum);
 	}
