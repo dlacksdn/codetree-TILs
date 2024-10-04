6108651047205937b08159acdd21d932cd8e5a97
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2024-10-04 16:01:05 +0900

    Title: Carry 피하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241004/Carry 피하기 2/escaping-carry-2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	vector<int> v(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+
+	int result = -1;
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			for (int k = j + 1; k < n; k++) {
+				int num1 = v[i];
+				int num2 = v[j];
+				int num3 = v[k];
+
+				while (true) {
+					if ((num1 % 10 + num2 % 10 + num3 % 10) >= 10)
+						break;
+
+					num1 /= 10;
+					num2 /= 10;
+					num3 /= 10;
+
+					if (num1 == 0 || num2 == 0 || num3 == 0) {
+						result = max(result, v[i] + v[j] + v[k]);
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	cout << result;
+}
