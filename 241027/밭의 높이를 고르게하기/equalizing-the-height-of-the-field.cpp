d41fe4e94086111e42d5ef551dad87575a4b6c5b
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2024-10-27 23:23:27 +0900

    Title: 밭의 높이를 고르게하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241027/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.cpp
@@ -0,0 +1,31 @@
+#include <iostream>	
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int N; // 밭의 높낮이 개수
+	int H; // 목표 높이
+	int T; // 최소 연속 횟수
+
+	cin >> N >> H >> T;
+
+	vector<int> v(N, 0);
+	
+	for (int i = 0; i < N; i++) {
+		cin >> v[i];
+	}
+
+	int result = INT_MAX;
+	for (int i = 0; i < N - T + 1; i++) {
+		int sum = 0;
+		for (int j = i; j < i + T; j++) {
+			int num = abs(v[j] - H);
+			sum += num;
+		}
+
+		result = min(result, sum);
+	}
+
+	cout << result << "\n";
+}
