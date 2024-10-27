e9f671c0a769f91f1129d1852c941ce2e1aeb7c4
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2024-10-27 20:01:09 +0900

    Title: 특정 구간의 원소 평균값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241027/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.cpp
+++ 241027/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.cpp
@@ -16,17 +16,19 @@ int main() {
 	int count = 0;
 	for (int i = 1; i <= N; i++) {
 		for (int j = i; j <= N; j++) {
-
 			double avg = 0;
 			int sum = 0;
+
 			for (int k = i; k <= j; k++) {
 				sum += v[k];
 			}
 
 			avg = (double)(sum) / (j - i + 1);
 
-			auto it = find(v.begin(), v.end(), avg);
-			if (it != v.end()) count++;
+			if (sum % (j - i + 1) == 0) { // 정수가 아니면 볼 필요도 없다
+				auto it = find(v.begin() + i, v.begin() + j + 1, avg); // 끝 인덱스까지 보고 싶으면 +1 해야 된다.
+				if (it != v.begin() + j + 1) count++;
+			}
 		}
 	}
 
