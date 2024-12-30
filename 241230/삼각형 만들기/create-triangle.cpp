f8c5647d375ed55aa8700da5da7e04d8bb5e8e7c
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2024-12-30 23:56:15 +0900

    Title: 삼각형 만들기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241230/삼각형 만들기/create-triangle.cpp
+++ 241230/삼각형 만들기/create-triangle.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
 using namespace std;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    return 0;
+	int N;
+	cin >> N;
+
+	vector<pair<int, int>> v;
+
+	for (int i = 0; i < N; i++) {
+		int x, y;
+		cin >> x >> y;
+		v.push_back({ x, y });
+	}
+
+	int result = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = i + 1; j < N; j++) {
+			for (int k = j + 1; k < N; k++) {
+				int num1 = (v[i].first * v[j].second) + (v[j].first * v[k].second) + (v[k].first * v[i].second);
+				int num2 = (v[j].first * v[i].second) + (v[k].first * v[j].second) + (v[i].first * v[k].second);
+
+				result = max(result, num1 - num2);
+			}
+		}
+	}
+
+	cout << result << "\n";
 }
