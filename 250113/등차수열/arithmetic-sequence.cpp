7009c1e4c7df56eb134cc683191164d20ec0d863
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2025-01-13 01:39:59 +0900

    Title: 등차수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250113/등차수열/arithmetic-sequence.cpp
+++ 250113/등차수열/arithmetic-sequence.cpp
@@ -13,19 +13,13 @@ int main() {
 	for (auto& p : v) cin >> p;
 	sort(v.begin(), v.end());
 
-	int max = (v.back() + *v.begin()) / 2;
+	int max = (v.back() + v[v.size() - 2]) / 2;
 
 	vector<int> list(max + 1);
 	for (int i = 0; i < n; i++) {
 		for (int j = i + 1; j < n; j++) {
-
-			for (int k = 1; k <= max; k++) {
-				if ((v[i] + v[j]) % 2 != 0) break;
-
-				if ((v[i] + v[j]) / 2 == k) {
-					list[k]++;
-				}
-			}
+			if ((v[i] + v[j]) % 2 != 0) continue;
+			list[(v[i] + v[j]) / 2]++;
 		}
 	}
 
