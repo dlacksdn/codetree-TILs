0b20f9d764db5cd260a8a994e1ea649d89b55038
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2025-01-12 21:57:02 +0900

    Title: 데이터센터의 온도 조정 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250112/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int N; // 장비 개수
+	int C, G, H; // 작업량
+
+	cin >> N >> C >> G >> H;
+
+	vector<pair<int, int>> v(N);
+	int TaMin = 1000;
+	int TaMax = 0;
+	int TbMin = 1000;
+	int TbMax = 0;
+
+	for (auto& p : v) {
+		cin >> p.first >> p.second;
+
+		if (p.first < TaMin) TaMin = p.first;
+		if (p.first > TaMax) TaMax = p.first;
+		if (p.second < TbMin) TbMin = p.second;
+		if (p.second > TbMax) TbMax = p.second;
+	}
+
+	int result = 0;
+	for (int i = TaMin - 1; i <= TbMax + 1; i++) {
+		int value = 0;
+		for (auto& p : v) {
+			if (i < p.first) {
+				value += C;
+			}
+			else if (p.first <= i && i <= p.second) {
+				value += G;
+			}
+			else {
+				value += H;
+			}
+		}
+
+		result = max(result, value);
+	}
+	
+	cout << result;
+}
