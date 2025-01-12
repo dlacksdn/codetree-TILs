b14a1744e50cc6b7200b5d81cd3d271089e63257
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2025-01-13 00:08:34 +0900

    Title: 정보에 따른 숫자 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250113/정보에 따른 숫자 2/number-based-on-information-2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int T, a, b;
+	cin >> T >> a >> b;
+
+	vector<int> S_location;
+	vector<int> N_location;
+
+	for (int i = 0; i < T; i++) {
+		char c;
+		int n;
+
+		cin >> c >> n;
+
+		if (c == 'S') {
+			S_location.push_back(n);
+		}
+		else if (c == 'N') {
+			N_location.push_back(n);
+		}
+	}
+
+	int count = 0;
+	for (int target = a; target <= b; target++) {
+		int S_near = INT_MAX;
+		int N_near = INT_MAX;
+
+		for (int i = 0; i < S_location.size(); i++) {
+			int num = abs(target - S_location[i]);
+
+			S_near = min(S_near, num);
+		}
+
+		for (int i = 0; i < N_location.size(); i++) {
+			int num = abs(target - N_location[i]);
+
+			N_near = min(N_near, num);
+		}
+
+		if (S_near <= N_near) count++;
+	}
+
+	cout << count;
+}
