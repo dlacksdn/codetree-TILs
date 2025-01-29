d25e9152220a46c74de72aa4acdb19ddfe5ae123
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2025-01-30 01:39:12 +0900

    Title: 3개의 선 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250130/3개의 선 2/three-lines-2.cpp
+++ 250130/3개의 선 2/three-lines-2.cpp
@@ -1,29 +1,44 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
-
-
 
 int main() {
 	int N;
 	cin >> N;
 
-	vector<int> x_arr;
-	vector<int> y_arr;
+	int x_arr[11] = { 0, };
+	int y_arr[11] = { 0, };
 
 	for (int i = 0; i < N; i++) {
 		int x, y;
 		cin >> x >> y;
 
-		auto it_x = find(x_arr.begin(), x_arr.end(), x);
-		auto it_y = find(y_arr.begin(), y_arr.end(), y);
+		x_arr[x]++;
+		y_arr[y]++;
+	}
 
-		if (it_x == x_arr.end() && it_y == y_arr.end()) {
-			x_arr.push_back(x);
-			y_arr.push_back(y);
+	vector<int> v;
+
+	for (int i = 0; i < 11; i++) {
+		v.push_back(x_arr[i]);
+	}
+	for (int i = 0; i < 11; i++) {
+		v.push_back(y_arr[i]);
+	}
+
+	bool flag = false;
+	for (int i = 0; i < 22; i++) {
+		for (int j = i + 1; j < 22; j++) {
+			for (int k = j + 1; k < 22; k++) {
+				int value = v[i] + v[j] + v[k];
+
+				if (value >= N) {
+					cout << 1;
+					return 0;
+				}
+			}
 		}
 	}
 
-	cout << (x_arr.size() <= 3);
+	cout << 0;
 }
