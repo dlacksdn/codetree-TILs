b0ad3bfff0c82a279aab9a77095e3163333b7607
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2025-02-16 17:14:43 +0900

    Title: 이상한 폭탄 3, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250216/이상한 폭탄 3/strange-bomb-3.cpp
+++ 250216/이상한 폭탄 3/strange-bomb-3.cpp
@@ -3,6 +3,13 @@
 #include <unordered_map>
 using namespace std;
 
+void markExplosion(vector<pair<int, bool>>& bomb_num, unordered_map<int, int>& m, int index) {
+    if (!bomb_num[index].second) {
+        m[bomb_num[index].first]++;
+        bomb_num[index].second = true;
+    }
+}
+
 int main() {
     int N, K;
     cin >> N >> K;
@@ -17,15 +24,8 @@ int main() {
     for (int i = 0; i <= N - K - 1; i++) { // i : 기준 폭탄의 인덱스
         for (int j = 1; j <= K; j++) { // j : 기준 폭탄에서의 거리
             if (bomb_num[i].first == bomb_num[i + j].first) { // 주어진 거리내의 폭탄이 번호가 같은 상황
-                if (bomb_num[i].second == false) { // 폭탄이 터진 적이 없다면 == 점수 산정을 한 적이 없다면
-                    m[bomb_num[i].first]++;
-                    bomb_num[i].second = true;
-                }
-
-                if (bomb_num[i + j].second == false) { // 폭탄이 터진 적이 없다면 == 점수 산정을 한 적이 없다면
-                    m[bomb_num[i + j].first]++;
-                    bomb_num[i + j].second = true;
-                }
+                markExplosion(bomb_num, m, i);
+                markExplosion(bomb_num, m, i + j);
             }
         }
     }
