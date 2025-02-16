4a392fdf78ac2a33ac24ec6d468f05bf886d074a
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2025-02-16 20:58:40 +0900

    Title: 구간 잘 나누기, Time: 1056ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 250216/구간 잘 나누기/divide-sections-well.cpp
+++ 250216/구간 잘 나누기/divide-sections-well.cpp
@@ -1,60 +1,62 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-vector<int> list; // 문제에서 주어진 수열
-int n; // 숫자 범위
-int m; // 숫자 개수 
-
-void process(vector<int>& v, int& result) {
-    int sum = 0;
-    int index = 0;
-    int max_num = 0;
-    for (int i = 0; i < n; i++) { // i : list를 쭉 순회하는 인덱스
-        sum += list[i];
-
-        if (v.size() != index && i == v[index]) { // 현 list의 인덱스가 칸막이까지 온 상황
-            index++;
-            max_num = max(max_num, sum);
-            sum = 0;
-        }
-    }
-
-    max_num = max(max_num, sum);
-
-    result = min(result, max_num);
-}
-
-void backTracking(vector<int>& v, int start, int range, int size, int& result) {
-    if (v.size() == size) {
-        process(v, result);
-        return;
-    }
-
-    for (int i = start; i <= range; i++) {
-        v.push_back(i);
-        backTracking(v, i + 1, range, size, result);
-        v.pop_back();
-    }
-}
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    int n = 5; // 숫자 범위 /  N개의 수가 주어지는거니까 0 ~ n-1이 맞다
+    int m = 3; // 숫자 개수 / m개의 구간이니 m-1개의 숫자를 골라서 칸막이 설치
 
     cin >> n >> m;
+    m--;
+
+    vector<int> list(n);
     for (int i = 0; i < n; i++) {
-        int num;
-        cin >> num;
-        list.push_back(num);
+        cin >> list[i];
     }
 
-    int result = 100;
-    vector<int> vec;
+    vector<int> v(m); // 칸막이
 
-    backTracking(vec, 0, n, m - 1, result);
+    for (int i = 0; i < m; i++) { // 벡터 초기값 설정
+        v[i] = i;
+    }
+
+
+
+    int result = 100;
+    while (true) {
+        
+        int sum = 0;
+        int index = 0;
+        int max_num = 0;
+        for (int i = 0; i < n; i++) { // i : list를 쭉 순회하는 인덱스
+            sum += list[i];
+
+            if (v.size() != index && i == v[index]) { // 현 list의 인덱스가 칸막이까지 온 상황
+                index++;
+                max_num = max(max_num, sum);
+                sum = 0;
+            }
+        }
+
+        max_num = max(max_num, sum);
+
+        result = min(result, max_num);
+
+        int i;
+        for (i = m - 1; i >= 0; i--) { // 벡터의 맨 뒤 인덱스부터 살펴보겠다
+            if (v[i] < n - m + i) { // 현재 위치의 숫자를 +1 할 수 있다
+                v[i]++;
+
+                for (int j = i + 1; j < m; j++) {
+                    v[j] = v[j - 1] + 1;
+                }
+
+                break;
+            }
+        }
+
+        if (i < 0) break;
+    }
 
     cout << result;
 }
