7c601df1c052e75f7614b18ab3dfe5c00116a163
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2025-02-17 22:38:40 +0900

    Title: 최대 H 점수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250217/최대 H 점수 2/maximum-h-score-2.cpp
+++ 250217/최대 H 점수 2/maximum-h-score-2.cpp
@@ -2,67 +2,38 @@
 #include <vector>
 using namespace std;
 
-void decide(vector<int> v, vector<int>& plus, int H, bool& flag) { // H점수가 가능한지 판단하는 함수
-    for (int i = 0; i < plus.size(); i++) {
-        v[plus[i]]++;
-    }
-
-    int count = 0;
-    for (int i = 0; i < v.size(); i++) {
-        if (v[i] >= H)
-            count++;
-    }
-
-    if (count >= H) {
-        flag = true;
-    }
-}
-
 int main() {
-    int N; // 주어지는 숫자의 개수
-    int L; // +1을 하는 원소의 개수
+    int N; // 숫자의 개수
+    int L; // 최대 몇 개까지 +1이 가능한가
 
     cin >> N >> L;
 
-    vector<int> v(N); // 입력받은 수열
+    vector<int> v(N); // 주어진 수열
+
     for (int i = 0; i < N; i++) {
         cin >> v[i];
     }
 
-    vector<int> plus(L);
-    
-
-    bool flag = false;
     for (int i = N; i >= 0; i--) { // i : 임의의 H점수
-        for (int j = 0; j < L; j++) {
-            plus[j] = j;
+        int origin = 0; // 원래 H보다 높은 숫자의 개수
+        int plus = 0; // +1을 하면 H가 되는 숫자의 개수
+
+        for (int j = 0; j < N; j++) { // j : 벡터 v를 순회하는 인덱스
+            if (v[j] >= i) { // 해당 원소가 H 이상인 경우
+                origin++;
+            }
+            else if (v[j] + 1 == i) { // +1을 하면 H가 되는 경우
+                plus++;
+            }
         }
 
-        while (true) {
-            decide(v, plus, i, flag);
-            if (flag) {
-                cout << i;
-                return 0;
-            }
-
-            int j;
-            for (j = L - 1; j >= 0; j--) { // j : 벡터 plus를 순회하는 인덱스
-                if (N - L + j <= plus[j]) { // 더 이상 올릴 수 없음
-                    continue;
-                }
-
-                plus[j]++;
-
-                for (int k = j + 1; k < L; k++) {
-                    plus[k] = plus[k - 1] + 1;
-                }
-
-                break;
-            }
-
-            if (j < 0) {
-                break;
-            }
+        if (origin >= i) { // 원래 숫자 만으로도 H점수 조건 만족
+            cout << i;
+            return 0;
+        }
+        else if (origin + plus >= i && plus <= L) { // H점수 조건을 만족하고 +1을 한 숫자가 L개를 넘기지 않은 경우 -> 정답
+            cout << i;
+            return 0;
         }
     }
 }
