3ff2ca9735a72fa596a712130fc7d652de6c54fd
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2025-02-18 16:49:03 +0900

    Title: 최대 최소간의 차, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250218/최대 최소간의 차/difference-between-maximum-and-minimum.cpp
+++ 250218/최대 최소간의 차/difference-between-maximum-and-minimum.cpp
@@ -23,8 +23,10 @@ int main() {
 
         for (int j = 0; j < N; j++) { // j : 벡터 v를 순회하는 인덱스
             if (!(i <= v[j] && v[j] <= i + K)) { // 해당 인덱스의 원소가 범위에 있지 않은 경우
-                int dis = min(abs(v[j] - i), abs(v[j] - (i + K)));
-                count += dis;
+                if (v[j] < i)
+                    count += i - v[j];
+                else if (i + K < v[j])
+                    count += v[j] - (i + K);
             }
         }
 
