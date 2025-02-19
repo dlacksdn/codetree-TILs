ae2509507aa7a354a82a183fdb6bf7b13a533a95
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2025-02-19 17:53:42 +0900

    Title: 비둘기와 전기줄, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250219/비둘기와 전기줄/pigeons-and-electric-cords.cpp
+++ 250219/비둘기와 전기줄/pigeons-and-electric-cords.cpp
@@ -16,13 +16,9 @@ int main() {
         if (v[index] == 2) { // 입력받은 비둘기가 언급 된 적이 없다
             v[index] = num;
         }
-        else if (v[index] == 0 && num == 1) { // 비둘기가 위치를 옮김
+        else if (v[index] != num) { // 비둘기가 위치를 옮김
             count++;
-            v[index] = 1;
-        }
-        else if (v[index] == 1 && num == 0) { // 비둘기가 위치를 옮김
-            count++;
-            v[index] = 0;
+            v[index] = num;
         }
     }
 
