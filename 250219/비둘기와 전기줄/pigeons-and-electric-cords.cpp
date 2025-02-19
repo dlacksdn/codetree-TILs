a00a94a41a483f88b5ff56e74d3f4607284198ff
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2025-02-19 17:50:44 +0900

    Title: 비둘기와 전기줄, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250219/비둘기와 전기줄/pigeons-and-electric-cords.cpp
+++ 250219/비둘기와 전기줄/pigeons-and-electric-cords.cpp
@@ -6,7 +6,7 @@ int main() {
     int N;
     cin >> N;
 
-    vector<int> v(N + 1, 2); // 0: 왼쪽, 1: 오른쪽, 2: 언급이 안 됨
+    vector<int> v(11, 2); // 0: 왼쪽, 1: 오른쪽, 2: 언급이 안 됨
 
     int count = 0;
     for (int i = 0; i < N; i++) {
