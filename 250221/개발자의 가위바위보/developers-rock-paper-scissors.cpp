5216a1b2ee8358169604d0b72741ebd6227f25cc
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2025-02-21 22:20:18 +0900

    Title: 개발자의 가위바위보, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250221/개발자의 가위바위보/developers-rock-paper-scissors.cpp
+++ 250221/개발자의 가위바위보/developers-rock-paper-scissors.cpp
@@ -3,10 +3,6 @@
 #include <algorithm>
 using namespace std;
 
-
-
-
-
 int main() {
 	int N;
 	cin >> N;
@@ -16,22 +12,22 @@ int main() {
 	for (int i = 0; i < N; i++) {
 		cin >> v[i].first >> v[i].second;
 	}
-	
+
 	int result = 0;
 	int count = 0;
-	// 1. 가위 2. 바위 3. 보
 	for (int i = 0; i < N; i++) {
-		if (v[i].first == 2 && v[i].second == 1 || 
-			v[i].first == 1 && v[i].second == 3 || 
+		if (v[i].first == 1 && v[i].second == 3 ||
+			v[i].first == 2 && v[i].second == 1 ||
 			v[i].first == 3 && v[i].second == 2)
 		{
 			count++;
 		}
 	}
-	result = max(result, count);
+
+	result = count;
+
 	count = 0;
 
-	// 1. 가위 2. 보 3. 바위
 	for (int i = 0; i < N; i++) {
 		if (v[i].first == 3 && v[i].second == 1 ||
 			v[i].first == 1 && v[i].second == 2 ||
@@ -40,64 +36,8 @@ int main() {
 			count++;
 		}
 	}
-	result = max(result, count);
-	count = 0;
 
-	// 1. 바위 2. 가위 3. 보
-	for (int i = 0; i < N; i++) {
-		if (v[i].first == 1 && v[i].second == 2 ||
-			v[i].first == 2 && v[i].second == 3 ||
-			v[i].first == 3 && v[i].second == 1)
-		{
-			count++;
-		}
-	}
 	result = max(result, count);
-	count = 0;
-
-	// 1. 보 2. 가위 3. 바위
-	for (int i = 0; i < N; i++) {
-		if (v[i].first == 3 && v[i].second == 2 ||
-			v[i].first == 2 && v[i].second == 1 ||
-			v[i].first == 1 && v[i].second == 3)
-		{
-			count++;
-		}
-	}
-	result = max(result, count);
-	count = 0;
-
-	// 1. 바위 2. 보 3. 가위
-	for (int i = 0; i < N; i++) {
-		if (v[i].first == 1 && v[i].second == 3 ||
-			v[i].first == 3 && v[i].second == 2 ||
-			v[i].first == 2 && v[i].second == 1)
-		{
-			count++;
-		}
-	}
-	result = max(result, count);
-	count = 0;
-
-	// 1. 보 2. 바위 3. 가위
-	for (int i = 0; i < N; i++) {
-		if (v[i].first == 2 && v[i].second == 3 ||
-			v[i].first == 3 && v[i].second == 1 ||
-			v[i].first == 1 && v[i].second == 2)
-		{
-			count++;
-		}
-	}
-	result = max(result, count);
-	count = 0;
 
 	cout << result;
 }
-
-/*
-바위 가위
-가위 보
-보 바위
-
-
-*/
