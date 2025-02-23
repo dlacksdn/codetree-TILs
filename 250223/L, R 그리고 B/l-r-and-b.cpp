6880b23c3b42ad6d623f8f0d9e68e9b26e8beea5
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2025-02-23 17:26:49 +0900

    Title: L, R 그리고 B, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250223/L, R 그리고 B/l-r-and-b.cpp
+++ 250223/L, R 그리고 B/l-r-and-b.cpp
@@ -29,14 +29,10 @@ int main() {
     }
 
     int result = 0;
-    //if (((L.first == R.first && R.first == B.first)) && ((L.second < R.second && R.second < B.second) || (B.second < R.second && R.second < L.second))) { // L R B가 한 줄에 있음
-    //    result = abs(L.first - B.first) + abs(L.second - B.second) + 1;
-    //}
-
-    if (((L.first == R.first && R.first == B.first)) && R.second > min(L.second, B.second) && R.second < max(L.second, B.second)) { // L R B가 한 줄에 있음
+    if (((L.first == R.first && R.first == B.first)) && (R.second > min(L.second, B.second)) && R.second < max(L.second, B.second)) { // L R B가 한 줄에 있음
         result = abs(L.first - B.first) + abs(L.second - B.second) + 1;
     }
-    else if (((L.second == R.second && R.second == B.second)) && ((L.first < R.first && R.first < B.first) || (B.first < R.first && R.first < L.first))) {
+    else if (((L.second == R.second && R.second == B.second)) && (R.first > min(L.first, B.first)) && R.first < max(L.first, B.first)) {
         result = abs(L.first - B.first) + abs(L.second - B.second) + 1;
     }
     else {
