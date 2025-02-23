93bdbd5725b45f980e3beea976f2965eacffd5e5
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2025-02-23 16:59:07 +0900

    Title: L, R 그리고 B, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250223/L, R 그리고 B/l-r-and-b.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<string> v(10);
+
+    pair<int, int> L;
+    pair<int, int> R;
+    pair<int, int> B;
+
+    for (int i = 0; i < 10; i++) {
+        cin >> v[i];
+
+        for (int j = 0; j < 10; j++) {
+            if (v[i][j] == 'L') {
+                L.first = i;
+                L.second = j;
+            }
+            else if (v[i][j] == 'R') {
+                R.first = i; 
+                R.second = j;
+            }
+            else if (v[i][j] == 'B') {
+                B.first = i;
+                B.second = j;
+            }
+        }
+    }
+
+    int result = 0;
+    if (L.first == R.first == B.first  || L.second == R.second == B.second) { // L R B가 한 줄에 있음
+        result = abs(L.first - B.first) + abs(L.second - B.second) + 1;
+    }
+    else {
+        result = abs(L.first - B.first) + abs(L.second - B.second) - 1;
+    }
+
+    cout << result;
+}
