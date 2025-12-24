a884a4fa0c283ec0ebf844d425589b79bc1f5061
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2025-12-24 16:19:55 +0900

    Title: 기수 정렬 구현, Time: 36ms, Memory: 1MB, Status: Passed - Codetree

--- 251224/기수 정렬 구현/implement-radix-sort.cpp
+++ 251224/기수 정렬 구현/implement-radix-sort.cpp
@@ -12,35 +12,26 @@ int main() {
         cin >> arr[i];
     }
 
-
+    int p = 1;
     for(int pos = 5; pos >= 0; pos--) {
         vector<vector<int>> arr_new(10);
         string str;
         char c;
 
         for(int i = 0; i < n; i++) {
-            str = to_string(arr[i]);
-
-            int size = str.size();
-            for(int i = 0; i < 6 - size; i++) {
-                str.insert(0, "0");
-            }
-
-
-            c = str[pos];
-            int digit = c - '0';
+            int digit = (arr[i] / p) % 10;
 
             arr_new[digit].push_back(arr[i]);
         }
 
-        vector<int> store_arr;
+        int index = 0;
         for(int i = 0; i < 10; i++) {
             for(int j = 0; j < arr_new[i].size(); j++) {
-                store_arr.push_back(arr_new[i][j]);
+                arr[index++] = arr_new[i][j];
             }
         }
 
-        arr = store_arr;
+        p *= 10;
     }
 
     for (int i = 0; i < n; i++) {
