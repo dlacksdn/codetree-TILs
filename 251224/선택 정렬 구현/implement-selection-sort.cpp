1085cb4ff0beeb6a0153a9ad82324af70c359a66
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2025-12-24 10:14:13 +0900

    Title: 선택 정렬 구현, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251224/선택 정렬 구현/implement-selection-sort.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    bool flag;
+    for(int i = 0; i < n-1; i++) {
+        int min = i;
+        flag = true;
+
+        for(int j = i + 1; j < n; j++) {
+            if(arr[j] < arr[min]) {
+                min = j;
+                flag = false;
+            }
+        }
+
+        if(!flag) {
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
