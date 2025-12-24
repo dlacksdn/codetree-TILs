eb52f7ad671527467ea8045f3ab87f48c896ccd0
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2025-12-24 15:54:16 +0900

    Title: 기수 정렬 구현, Time: 21ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 251224/기수 정렬 구현/implement-radix-sort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+
+    for(int pos = 5; pos >= 0; pos--) {
+        vector<vector<int>> arr_new(10);
+        string str;
+        char c;
+
+        for(int i = 0; i < n; i++) {
+            str = to_string(arr[i]);
+            c = str[pos];
+            int digit = c - '0';
+
+            arr_new[digit].push_back(arr[i]);
+        }
+
+        vector<int> store_arr;
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < arr_new[i].size(); j++) {
+                store_arr.push_back(arr_new[i][j]);
+            }
+        }
+
+        arr = store_arr;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
