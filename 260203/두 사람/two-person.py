08313450d5acf14900e24947f5cce98131d6c889
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2026-02-03 12:36:10 +0900

    Title: 두 사람, Time: 72ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 260203/두 사람/two-person.py
@@ -0,0 +1,10 @@
+a, b = input().split()
+a = int(a)
+
+c, d = input().split()
+c = int(c)
+
+if (a < 19 and b < 19 or b == 'W' and d == 'W'):
+    print(0)
+elif (a >= 19 and b == 'M' or c >= 19 and d == 'M'):
+    print(1)
