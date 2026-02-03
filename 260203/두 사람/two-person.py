2df8127f6a4b16e2891a897ac5cc49aa0dd43b44
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2026-02-03 12:37:11 +0900

    Title: 두 사람, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260203/두 사람/two-person.py
+++ 260203/두 사람/two-person.py
@@ -4,7 +4,7 @@ a = int(a)
 c, d = input().split()
 c = int(c)
 
-if (a < 19 and b < 19 or b == 'W' and d == 'W'):
+if (a < 19 and c < 19 or b == 'W' and d == 'W'):
     print(0)
 elif (a >= 19 and b == 'M' or c >= 19 and d == 'M'):
     print(1)
